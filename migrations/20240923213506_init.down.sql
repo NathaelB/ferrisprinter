2ee2e6ad11a53f91b66a8516c685d4dd294a4375
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-09-30 17:11:45 +0200

    feat: Add down migration script for access_tokens table
    This commit adds a down migration script for the `access_tokens` table in the database. It drops the table if it exists. This change is necessary to ensure the database schema is in sync with the codebase.
    
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- migrations/20240923213506_init.down.sql
+++ migrations/20240923213506_init.down.sql
@@ -1,2 +1,4 @@
 -- Add down migration script here
 DROP TABLE IF EXISTS refresh_tokens;
+
+DROP TABLE IF EXISTS access_tokens;
--- migrations/20240923213506_init.up.sql
+++ migrations/20240923213506_init.up.sql
@@ -5,3 +5,11 @@ CREATE TABLE refresh_tokens (
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL
 );
+
+CREATE TABLE access_tokens (
+    id UUID PRIMARY KEY,
+    serial_number VARCHAR(255) NOT NULL UNIQUE,
+    token TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
