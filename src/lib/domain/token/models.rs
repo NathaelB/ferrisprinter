185661e7bcdf7d5ffd9a2f366fbcc7b7fccaad2d
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-09-30 15:53:41 +0200

    feat: Add AccessToken model and repository
    The code changes introduce a new model called `AccessToken` in the `token` module. This model represents an access token and includes fields such as `id`, `serial_number`, `token`, `created_at`, and `updated_at`. Additionally, a repository trait `AccessTokenRepository` is added to handle the creation of access tokens. This commit enhances the token domain by providing a structured representation of access tokens and a repository for managing them.
    
    ```
    
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/lib/domain/token/models.rs
+++ src/lib/domain/token/models.rs
@@ -1,2 +1,3 @@
+pub mod access_token;
 pub mod refresh_token;
 pub mod token;
--- src/lib/domain/token/ports/access_token.rs
+++ src/lib/domain/token/ports/access_token.rs
@@ -1,2 +1,14 @@
-pub trait AccessTokenService: Send + Sync + Clone {}
-pub trait AccessTokenRepository: Send + Sync + Clone {}
+use std::future::Future;
+
+use crate::domain::token::models::access_token::{AccessToken, AccessTokenError};
+
+pub trait AccessTokenService: Send + Sync + Clone + 'static {
+  
+}
+pub trait AccessTokenRepository: Send + Sync + Clone + 'static {
+  fn create_access_token(
+    &self,
+    token: &str,
+    serial_number: &str,
+  ) -> impl Future<Output = Result<AccessToken, AccessTokenError>> + Send;
+}
